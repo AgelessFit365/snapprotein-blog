911093a88643096da61ad1333a6a967586b0e5d1
Robert Fowler <agelessfitness@protonmail.com>
2026-04-15 20:12:35 -0500

    exclude test posts from sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://snapprotein.com',
-  integrations: [sitemap()],
+  integrations: [sitemap({
+    filter: (page) => !page.includes('/test-blog/') && !page.includes('/test-post/')
+  })],
 });
